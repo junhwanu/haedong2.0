476cde0c439df2f4c99c79d45666ecc7562c67f6
Jinmo Jun <junhwanu@gmail.com>
2017-06-18 17:42:22 +0900

    Init commit.

--- Haedong.py
+++ Haedong.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 import sys, os, time, threading
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)).replace('\\','/') + '/module')
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)).replace('\\','/') + '/modules')
+import request_thread
