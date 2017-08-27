24cda52b4b7e0a8f6933e7284a7b023df666dd91
Jinmo Jun <junhwanu@gmail.com>
2017-08-27 17:52:23 +0900

    1. request_tick_candle 함수 추가

--- manager/db_manager.py
+++ manager/db_manager.py
@@ -72,5 +72,48 @@ class DBManager(ManagerClass):
     def get_name(self):
         return str(self.__class__.__name__)
 
+    def request_tick_candle(self, subject_code, tick_unit):
+        query = '''
+        select t1.id
+             , t1.date
+             , t1.high
+             , t1.low
+             , t2.price as open
+             , t3.price as close
+          from (
+                select Floor((result.row-1) / '%s') + 1 as id
+                     , min(date_format(result.date, '%%Y-%%m-%%d %%H:%%i:%%s')) as date
+                     , max(result.id) as max_id
+                     , min(result.id) as min_id
+                     , max(result.price) as high
+                     , min(result.price) as low
+                  from (
+                        select @rownum:=@rownum+1 as row
+                             , id
+                             , price
+                             , date
+                          from %s s1
+                         inner join (
+                                    select @rownum:=0
+                                      from dual
+                                    ) s2
+                       ) result
+                 group by Floor((result.row-1) / '%s')
+               ) t1
+         inner join %s t2
+            on t1.min_id = t2.id
+         inner join %s t3
+            on t1.max_id = t3.id
+        ;
+        ''' % (tick_unit, subject_code, tick_unit, subject_code, subject_code)
+
+        return self.exec_query(query, fetch_type=FETCH_ALL, cursor_type=CURSOR_DICT)
+
     def print_status(self):
         print(self.__getattribute__())
+
+if __name__ == '__main__':
+    dbm = DBManager()
+    result = dbm.request_tick_candle('GCJ17', 60)
+    for rs in result:
+        print(rs)
